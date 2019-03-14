f058bbf9e587f43a480f95f4ce3adc0ed30d282a
TATIANE PEREIRA BONFIM <509660204@alunos.etec.itaqua>
2019-03-14 20:13:05 -0300

    Adiçaõ passo 1

--- NoBullying.java
+++ NoBullying.java
@@ -1,5 +1,11 @@
 public class NoBullying {  
 	public static void main(String[] args){
 		System.out.println('Programa NoBullying');
+
+		System.out.println("Passo 1: Ver o comportamento da pessoa");
+
+		System.out.println("Passo 2: Ter respeito");
+
+		System.out.println("Passo 3: Realizar palestras em escolas falando sobre o assunto");
 	} 
 }
